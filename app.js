218f529d3f6a5a695d5fd62ca866aea95db5cc90
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-03 14:34:46 +0200

    experiments

---
+++ app.js
@@ -0,0 +1,48 @@
+const request = require('request')
+const apiToken = process.env.API_TOKEN
+const apiKey = process.env.API_KEY
+const boardId = process.env.BOARD_ID
+
+const listsFromBoard = (id) => {
+  return `https://api.trello.com/1/boards/${id}/lists?key=${apiKey}&token=${apiToken}`
+}
+
+const cardsFromBoard = (id) => {
+  return `https://api.trello.com/1/boards/${id}/cards?key=${apiKey}&token=${apiToken}`
+}
+
+const toJson = (response) => {
+  return JSON.parse(response.body)
+}
+
+const filterByName = (name) => {
+  return (lists) => {
+    return lists.filter((list) => list.name === name)
+  }
+}
+
+const get = (uri) => {
+  return new Promise((resolve, reject) => {
+    return request(uri, (error, response, body) => {
+      if (error) { reject(error) }
+      resolve(response)
+    })
+  })
+}
+
+get(cardsFromBoard(boardId))
+.then(toJson)
+.then(filterByName('Board Unica'))
+.then((cards) => {
+  console.log(cards)
+})
+
+// get(listsFromBoard(boardId))
+// .then(toJson)
+// .then(filterByName('DONE'))
+// .then(list => {
+//   console.log(list)
+// })
+// .catch((error) => {
+//   console.log(error)
+// })
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Trello API
+
+### Requirements
+- nodejs and npm installed
+
+### Setup
+1. copy `.env.example` in `.env` file
+2. fill the credential tokens
+
+### Run application
+```sh
+env $(cat .env | xargs) node app.js
+```
