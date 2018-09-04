94d5e67a478c7d4b8a13192fab0089b85eb9058e
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-04 12:57:50 +0200

    extractss trello api uris

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
+const {cardsFromBoard, actionsFromCard} = require('./urls')
 const {fetchJson} = require('./fetch')
 
-const apiToken = process.env.API_TOKEN
-const apiKey = process.env.API_KEY
 const boardId = process.env.BOARD_ID
 
-const listsFromBoard = (id) => {
-  return `https://api.trello.com/1/boards/${id}/lists?key=${apiKey}&token=${apiToken}`
-}
-
-const cardsFromBoard = (id) => {
-  return `https://api.trello.com/1/boards/${id}/cards?key=${apiKey}&token=${apiToken}`
-}
-
-const cardActions = (id) => {
-  return `https://api.trello.com/1/cards/${id}/actions?filter=updateCard:idList&key=${apiKey}&token=${apiToken}`
-}
-
-const filterByName = (name) => {
-  return (lists) => {
-    return lists.filter((list) => list.name === name)
-  }
-}
-
 const cardActionsRequestsFrom = (card) => {
-  return fetchJson(cardActions(card.id))
+  return fetchJson(actionsFromCard(card.id))
   .then((actions) => {
     return {card, actions}
   })
