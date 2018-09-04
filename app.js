cc4e399170f6d5984254fca73b31c5999e38647e
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-04 15:14:20 +0200

    refactors print rules

--- app.js
+++ app.js
@@ -14,14 +14,31 @@ const toCardActions = (cards) => {
   return Promise.all(cards.map(cardActionsRequestsFrom))
 }
 
+const print = (fn) => {
+  return (input) => {
+    let toPrint = fn(input)
+    console.log(toPrint)
+    return input
+  }
+}
+
+const listsMoveActionsText = (cardsActions) => {
+  return cardsActions.map((cardActions) => {
+    let head = '--------------------------------------\nCard: ' + cardActions.card.name
+
+    let actions = cardActions.actions.map((action) => {
+      return action.date + ': ' + action.data.listBefore.name + ' --> ' + action.data.listAfter.name
+    })
+
+    return [head, ...actions].join('\n')
+  }).join('\n')
+}
+
 fetchJson(cardsFromBoard(boardId))
 .then(toCardActions)
+.then(print(listsMoveActionsText))
 .then((cardsActions) => {
-  return cardsActions.forEach((cardActions) => {
-    console.log('--------------------------------------\nCard:', cardActions.card.name)
-    cardActions.actions.forEach((action) => {
-      console.log(action.date, ':', action.data.listBefore.name, '-->', action.data.listAfter.name)
-    })
-  })
+  // console.log('--------------------------------------')
+  // console.log(cardsActions)
 })
 .catch(err => console.log(err))
