031f60d6286e7790bb9252cebd791711af102acf
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-06 16:39:23 +0200

    failing smoke test

--- test/smoke/TrelloApi.js
+++ test/smoke/TrelloApi.js
@@ -1,12 +1,11 @@
 const {equal, notEqual} = require('assert')
 const {TrelloApi} = require('../../lib/trello/api')
+const {TrelloConfig} = require('../../lib/trello/config')
 
 test('TrelloApi', () => {
   test('#myBoards', () => {
     test('returns all boards with id and name', (done) => {
-      let trelloApi = TrelloApi()
-
-      trelloApi.myBoards()
+      TrelloApi().myBoards()
       .then((boards) => {
         boards.forEach((board) => {
           equal(typeof board, 'object')
@@ -15,8 +14,36 @@ test('TrelloApi', () => {
         })
 
         done()
+      }).catch(done)
+    })
+  })
+
+  test('#cardsOf', () => {
+    let boardId = TrelloConfig().boardId
+
+    test('returns all cards of a given board', (done) => {
+      TrelloApi().cardsOf(boardId)
+      .then((cards) => {
+        cards.forEach((card) => {
+          equal(typeof card, 'object')
+          notEqual(typeof card.id, 'undefined')
+          notEqual(typeof card.name, 'undefined')
+        })
+
+        done()
+      }).catch(done)
+    })
+
+    test('returns the change-list actions within the card', (done) => {
+      TrelloApi().cardsOf(boardId)
+      .then((cards) => {
+        cards.forEach((card) => {
+          equal(typeof card.actions.length, 'number')
+        })
+
+        done()
       })
-      .catch(done)
+    .catch(done)
     })
   })
 })
