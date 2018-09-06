0a8968be9ec42223f0887923fae11eebabae398a
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-06 11:19:00 +0200

    adds title to print function

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const {fetchJson} = require('./lib/fetch')
 const {closedByWeeks} = require('./lib/rules/closedByWeeks')
 const {listsMovesActions} = require('./lib/rules/listsMovesActions')
 
-const cardActionsRequestsFrom = (card) => {
-  return fetchJson(actionsFromCardUrl(card.id))
+const cardActionsRequestsFrom = (card, since) => {
+  return fetchJson(actionsFromCardUrl(card.id, since))
   .then((actions) => {
     return {card, actions}
   })
@@ -14,18 +14,34 @@ const toCardActions = (cards) => {
   return Promise.all(cards.map(cardActionsRequestsFrom))
 }
 
-const print = (fn) => {
+const print = (title, fn) => {
   return (input) => {
+    console.log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log(`~~~ ${title} ~~~`)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
     console.log(fn(input))
     return input
   }
 }
 
+const countArrayItems = (fn) => {
+  return function (input) {
+    let resultObject = fn(input)
+    let countObject = {}
+    Object.keys(resultObject).forEach(function (key) {
+      countObject[key] = resultObject[key].length
+    })
+
+    return countObject
+  }
+}
+
 fetchJson(cardsUrl)
 .then(toCardActions)
-.then(print(closedByWeeks))
-.then(print(listsMovesActions))
+.then(print('List Moves Actions', listsMovesActions))
+.then(print('Closed By Weeks Items', closedByWeeks))
+.then(print('Closed By Weeks Count', countArrayItems(closedByWeeks)))
 .then((cardsActions) => {
-  console.log('\n~ END ~\n')
+  console.log('\n\n~ END ~\n')
 })
 .catch(err => console.log(err))
