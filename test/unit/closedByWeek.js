299bcd3ec871c988eb4bf4291e194537c28a70ad
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-07 15:14:17 +0200

    adds test that returns empty array if there are no closed cards

--- test/unit/closedByWeek.js
+++ test/unit/closedByWeek.js
@@ -2,7 +2,19 @@ const { deepEqual } = require('assert')
 
 test('ClosedByWeek', () => {
   test('returns empty array if no cards present', (done) => {
-    let fakeCardsFor = () => Promise.resolve([])
+    let fakeCardsFor = cardsForWillReturn([])
+    let closedByWeek = ClosedByWeek(fakeCardsFor)
+
+    closedByWeek.get('boardId')
+    .then((cards) => {
+      deepEqual(cards, [])
+
+      done()
+    }).catch(done)
+  })
+
+  test('returns empty array if no card is closed', (done) => {
+    let fakeCardsFor = cardsForWillReturn([card(1)])
     let closedByWeek = ClosedByWeek(fakeCardsFor)
 
     closedByWeek.get('boardId')
@@ -14,8 +26,11 @@ test('ClosedByWeek', () => {
   })
 })
 
+const card = (id) => ({ id: `${id}`, name: 'first', actions: [] })
+const cardsForWillReturn = (arg) => () => Promise.resolve(arg)
+
 const ClosedByWeek = (cardsFor) => {
   return {
-    get: (boardId) => cardsFor(boardId)
+    get: (boardId) => cardsFor(boardId).then((cards) => cards.filter((card) => card.actions.length > 0))
   }
 }
