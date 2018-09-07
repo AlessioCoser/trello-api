92d566d30ff4c72f2f6f4830156498b482881f88
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-07 18:07:35 +0200

    refactors tests

--- test/unit/closedByWeekTest.js
+++ test/unit/closedByWeekTest.js
@@ -3,15 +3,11 @@ const { closedByWeek } = require('../../lib/rules/closedByWeek')
 
 test('closedByWeek', () => {
   test('returns empty array if no cards present', () => {
-    let cards = closedByWeek([])
-
-    deepEqual(cards, [])
+    deepEqual(closedByWeek([]), [])
   })
 
   test('returns empty array if no card is closed', () => {
-    let cards = closedByWeek([card(1)])
-
-    deepEqual(cards, [])
+    deepEqual(closedByWeek([card(1)]), [])
   })
 
   test('returns cards closed grouped by week', () => {
@@ -19,15 +15,15 @@ test('closedByWeek', () => {
     const MONDAY_WEEK_ONE = '2018-09-03T00:00:00.000Z'
     const FRIDAY_WEEK_ONE = '2018-09-07T00:00:00.000Z'
     const MONDAY_WEEK_TWO = '2018-09-10T00:00:00.000Z'
-    let cards = [
+
+    let closed = closedByWeek([
       card(1),
       cardClosedOn(11, SUNDAY_WEEK_ONE),
       cardClosedOn(12, MONDAY_WEEK_ONE),
       cardClosedOn(21, MONDAY_WEEK_TWO),
       cardClosedOn(13, FRIDAY_WEEK_ONE)
-    ]
+    ])
 
-    let closed = closedByWeek(cards)
     deepEqual(Object.keys(closed), ['2018-09-03', '2018-09-10'])
     deepEqual(closed['2018-09-03'].map((card) => card.id), [11, 12, 13])
     deepEqual(closed['2018-09-10'].map((card) => card.id), [21])
