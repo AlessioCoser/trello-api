51301a5a7cbab35dc204b2c6c0164c566f9af0ee
Alessio Coser <alessio.coser@xpeppers.com>
2018-09-24 15:29:20 +0200

    adds myBoards if no boardId is specified

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const { cardsFor } = require('./lib/trello')
+const { cardsFor, myBoards } = require('./lib/trello')
 const { configuration } = require('./lib/trello/configuration')
 const { closedByWeek } = require('./lib/rules/closedByWeek')
 
 const boardId = configuration().boardId
 
-cardsFor(boardId)
-.then(closedByWeek)
-.then(console.log)
+if (boardId) {
+  cardsFor(boardId)
+  .then(closedByWeek)
+  .then(console.log)
+} else {
+  console.log('Please choose one of these boards:\n\n')
+  myBoards().then(console.log)
+}
+
+
